aeb617f3afc87d4b516301599a450ae9be3bd2b5
Søs <slys87@gmail.com>
2021-09-30 11:21:56 +0200

    Last changes

--- src/CodeBreaker.java
+++ src/CodeBreaker.java
@@ -14,14 +14,45 @@ public class CodeBreaker {
 
 
         //Decoder//
-        String[] encodedStringArray = stringEncodedToNumbers.split("");
+        String[] encodedStringArray = stringEncodedToNumbers.split(stringEncodedToNumbers);
 
         String decodedString = numberCypherDecoder(encodedStringArray);
         String decodedStringResult = "The decoded message: " + decodedString;
         System.out.println(decodedStringResult);
 
+        //CaesarEncoder//
+        String caesarEncodedString = "Ohøj";
+        caesarEncodedString = caesarEncodedString.toLowerCase(Locale.ROOT);
+
+        String stringEncodedCaesar = ceasarEncoder(caesarEncodedString);
+
+        String caesarEncodeStringResult = "The encode caesar message: " + stringEncodedCaesar;
+        System.out.println(caesarEncodeStringResult);
+
+        //CaesarDecoder//
+        String caesarDecodedString = caesarDecoder(caesarEncodedString);
+        String caesarDecodedResult = "The decoded caesar message: " + caesarDecodedString;
+        System.out.println(caesarDecodedResult);
+
     }
     //Methods//
+
+    //Make the alphabet
+    static String alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
+
+    //Use methode to find characters index number
+    public static int charToIndexNum(char character) {
+        int charIndexNumbers = alphabet.indexOf(character) + 1;
+
+        return charIndexNumbers;
+    }
+
+    //Use methode to find index numbers character
+    public static char indexNumToChar(int index) {
+        char indexNumbersChar = alphabet.charAt(index - 1);
+
+        return indexNumbersChar;
+    }
 
     //Methode Encoder//
     public static String numberCypherEncoder(String encoder) {
@@ -29,18 +60,13 @@ public class CodeBreaker {
         //Type of return
         String stringToEncode = "";
 
-        //Make the alphabet
-        String alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
-
         //Run the string through a for loop
         for (int i = 0; i < encoder.length(); i++) {
             char stringToIndex = encoder.charAt(i);
-
-            //Give the alphabet some index numbers
-            int indexNumbers = alphabet.indexOf(stringToIndex) + 1;
+            int charIndex = alphabet.indexOf(stringToIndex) + 1;
 
             //Gonna add my type to my int
-            stringToEncode = stringToEncode + indexNumbers + ";";
+            stringToEncode = stringToEncode + charIndex + ";";
         }
 
         return stringToEncode;
@@ -52,24 +78,53 @@ public class CodeBreaker {
 
         String stringToDecode = "";
 
-        String alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
-
         for (int i = 0; i < decode.length; i++) {
             String indexToString = decode[i];
             int stringToInt = Integer.parseInt(indexToString) - 1;
 
-            int letterIndexNumbers = alphabet.charAt(stringToInt);
+            int indexToChar = alphabet.charAt(stringToInt);
 
-            stringToDecode = stringToDecode + letterIndexNumbers ++;
+            stringToDecode = stringToDecode + indexToChar ++;
         }
 
         return stringToDecode;
     }
 
+    //Methode CeasarEncoder//
     public static String ceasarEncoder(String message) {
 
-        int index = numberCypherEncoder(message);
+        String encodedString = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            char messageIndex = message.charAt(i);
+            int charIndex = charToIndexNum(messageIndex);
+
+            int indexShift = (charIndex + 6) % alphabet.length();
+            char encodedChar = indexNumToChar(indexShift);
+
+            encodedString = encodedString + encodedChar;
+        }
+        return encodedString;
     }
+
+    //Methode CaesarDecoder//
+    public static String caesarDecoder(String message) {
+
+        String decodedString = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            char messageIndex = message.charAt(i);
+            int charIndex = charToIndexNum(messageIndex);
+
+            int indexShift = (charIndex - 6) % alphabet.length();
+            char decodedChar = indexNumToChar(indexShift);
+
+            decodedString = decodedString + decodedChar;
+
+        }
+        return decodedString;
+    }
+
 
 
 }
